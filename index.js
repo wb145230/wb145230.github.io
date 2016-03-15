279723fff9213c9a37e313163812ed155e6a823d
wb145230 <wb145230@163.com>
2016-03-15 16:28:02 +0800

    45

--- index.js
+++ index.js
@@ -44,3 +44,9 @@ connectWebViewJavascriptBridge(function(bridge) {
 
     });    
 })
+
+function onLoad() {
+  var now = new Date().getTime();
+  var page_load_time = now - performance.timing.navigationStart;
+  alert("page loading time: " + page_load_time);
+}
